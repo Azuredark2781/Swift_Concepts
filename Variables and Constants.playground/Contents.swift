7335a2943febe34867390cccb9ecad87736981e8
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-10-18 14:23:53 +0530

    Variables and Constans Example added

---
+++ Variables and Constants.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// Implicatle - Implicitly it will take string type
+var name = "Mallikarjun"
+print(name)
+
+//Explicit - giving variable type
+
+var myName: String = "Mallikarjun Hanagandi"
