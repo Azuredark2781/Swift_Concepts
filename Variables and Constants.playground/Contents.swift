be581142790ca072189448f47b42087663622cf4
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-10-18 14:45:13 +0530

    variables - completed

--- Variables and Constants.playground/Contents.swift
+++ Variables and Constants.playground/Contents.swift
@@ -1,11 +1,56 @@
 import UIKit
 
-var str = "Hello, playground"
+/*  Variables and Constants:
+      - Constants and Variables must be declared before they used
+      - Constants are declared using let keyword
+      - Variables are declaured using var keyword
+ */
 
-// Implicatle - Implicitly it will take string type
+/* Type Annotations: You can provide a type annotations for a variable or constant, to be
+                     clear about that kind of the variable or constant can store.
+ 
+ */
+
+/*
+   var name; // Error: ype annotation missing in pattern
+   print(name)
+ 
+   var name = nil // 'nil' requires a contextual type
+   print(name)
+ 
+ */
+
+
+// Variables
+
+// Implicit - Implicitly it will take string type
 var name = "Mallikarjun"
 print(name)
 
 //Explicit - giving variable type
-
 var myName: String = "Mallikarjun Hanagandi"
+print(myName)
+
+myName = "Mallikarjun Hanagandi"
+//Ineger
+var age = 25
+print(age)
+
+//Fload and Double
+var cirucumference = 110.8
+print(cirucumference)
+
+//Boolean - True or False - Implicitlt
+var doorIsClosed:Bool = true
+print(doorIsClosed)
+
+//Boolean - True or False - Implicitlt
+var doorIsClosedExplicitly = false
+print(doorIsClosedExplicitly)
+
+
+
+
+//Constants
+
+
