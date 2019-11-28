cbe3a3b2725044849787bd8162bd866127fbdf82
Mallikarjun Hanagandi <mallikarjun.iphone9@gmail.com>
2019-11-28 14:57:23 +0530

    Sort()
    Sorting example added

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -1,5 +1,21 @@
 import UIKit
 
-var str = "Hello, playground"
+var names = ["Jemima", "Peter", "David", "Kelly", "Isabella"]
+names.sort()
 
-print(str)
+print(names) 
+//Output: ["David", "Isabella", "Jemima", "Kelly", "Peter"]
+
+var numbers = [0, 2, 3, 5, 10, 2]
+numbers.sort {
+  return $0 < $1 //ascending order
+}
+print(numbers)
+//Output: [0, 2, 2, 3, 5, 10]
+
+numbers.sort {
+  return $0 > $1 //descending order
+}
+print(numbers)
+//Output: [10, 5, 3, 2, 2, 0]
+
